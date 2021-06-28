d3796a8f64544980894cf416be9ee8eaf31d67e4
Neel Adwani <nprosofficial@gmail.com>
2021-06-28 14:11:33 +0000

    Added 2FA and OTP

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-from flask import Flask, render_template, request, make_response, redirect, url_for
+from flask import Flask, render_template, request,  redirect
 import smtplib
 from email.mime.multipart import MIMEMultipart
 import os
+import random
 
+
+
+def sendOTP():
+  sender_address = os.environ['email']
+  sender_pass = os.environ['pass']
+  receiver_address = 'nprosofficial@gmail.com'
+  otp = random.randint(1000, 9999)
+
+  message = MIMEMultipart()
+
+  message['From'] = sender_address
+  message['To'] = receiver_address
+  message['Subject'] = 'Your OTP is ' + str(otp)
+
+  session = smtplib.SMTP('smtp.gmail.com', 587)
+  session.starttls()
+  session.login(sender_address, sender_pass)
+  text = message.as_string()
+  session.sendmail(sender_address, receiver_address, text)
+  session.quit()
+  print('Mail Sent')
+  return otp
 
 
 
@@ -19,8 +42,7 @@ def index():
     email = request.form.get("email") 
     password = request.form.get("password")
     print("here")
-    if email == "lmao" and password == "blahaj":
-      
+    if email == "neeltr.n@gmail.com" and password == "blahaj":
       return redirect(url_for('second'))
   
   return render_template('index.html')
@@ -28,9 +50,18 @@ def index():
 
 
 # Creating different routes
-@app.route('/2fa')
+@app.route('/2fa', methods = ["GET", "POST"])
 def second():
-  return "I'm on a separate route"
+  a = sendOTP()
+  flag = 0
+  if request.method == "POST" and flag == 0:
+    otp = request.form.get("otp")
+    flag = 1
+    if a == otp:
+      return "Verified!"
+    else:
+      return "Incorrect OTP!"
+  return render_template('2fa.html')
 
 
 
